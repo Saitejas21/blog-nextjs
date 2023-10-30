54d1602b45f482de6b7b768789fee3ab882a9dda
Sai Tejas A R <46528244+Saitejas21@users.noreply.github.com>
2023-10-30 22:09:56 +0530

    dark/light mode ui design

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import styles from './themeToggle.module.css'
+import Image from 'next/image'
+
 
 function ThemeToggle() {
   return (
-    <div className={styles.container}>ThemeToggle</div>
+    <div className={styles.container}>
+      <Image src="/moon.png" alt="" width={14} height={14}/>
+      <div className={styles.ball}></div>
+      <Image src="/sun.png" alt="" width={14} height={14}/>
+      
+    </div>
   )
 }
 
