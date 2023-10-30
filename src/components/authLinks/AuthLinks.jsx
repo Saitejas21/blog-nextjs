fb1afb64db4ad9c31a4542ed9b7fd5c428abcf7f
Sai Tejas A R <46528244+Saitejas21@users.noreply.github.com>
2023-10-30 13:56:59 +0530

    login / logout component

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import styles from './authLinks.module.css'
+import Link from 'next/link'
 
 function AuthLinks() {
+  
+  const status="notauthenticated"
   return (
-    <div className={styles.container}>AuthLinks</div>
+    <>
+    {status==="notauthenticated" ? (
+      <Link href="/login">Login</Link>
+
+    ):(
+      <>
+      <Link href="/write">Write</Link>
+      <span className={styles.link}>Logout</span>
+      </>
+    )}
+    </>
   )
 }
 
