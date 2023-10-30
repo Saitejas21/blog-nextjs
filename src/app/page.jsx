9066731cbc558f70011e86c975bdf8802a7abf0f
Sai Tejas A R <46528244+Saitejas21@users.noreply.github.com>
2023-10-30 13:15:19 +0530

    components added to main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,20 @@ import Navbar from "@/components/navbar/Navbar";
 import styles from "./homepage.module.css";
 import Link from "next/link";
 import Footer from "@/components/footer/Footer";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/CardList/CardList";
+import Menu from "@/components/Menu/Menu";
 
 export default function Home() {
-  return <div>
+  return <div className={styles.container}>
+    <Featured/>
+    <CategoryList/>
+    <div className={styles.content}>
+      <CardList/>
+      <Menu/>
+
+    </div>
   
     </div>;
 }
