9d52c87f43471ddd7761558444e867c5dd28f333
Sai Tejas A R <46528244+Saitejas21@users.noreply.github.com>
2023-10-30 11:49:11 +0530

    init: created components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,11 @@
+import Navbar from "@/components/navbar/Navbar";
 import styles from "./homepage.module.css";
 import Link from "next/link";
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
   return <div>
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing eli</h1>
-    <Link href="/">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, nam distinctio? Dolorem cupiditate fuga magnam a suscipit, officiis eveniet quas, 
-    mollitia ipsum culpa eius maxime dignissimos ducimus! Quaerat minus ab fugiat repellendus voluptatem similique quod ut distinctio doloremque, 
-    rem omnis voluptates soluta facilis cupiditate numquam exercitationem asperiores tempore eveniet, in cum, fugit recusandae? Debitis ab distinctio, 
-    necessitatibus nesciunt sequi eius rem laboriosam sed dignissimos. Rem reprehenderit eaque velit. Rerum, aliquam consectetur quam pariatur sit quidem error vel 
-    atque fugit totam, officia molestiae aut, nihil amet saepe optio voluptate iste voluptatum tempore quae nisi ea. Eum natus explicabo molestias dolore nihil repellendus
-     quaerat itaque! Eius, et. Labore optio expedita vero iusto totam numquam maiores adipisci perferendis nulla nisi commodi non excepturi amet vitae esse, ducimus veniam 
-     itaque rerum. Reprehenderit eius perspiciatis corporis ullam? Explicabo earum ducimus accusamus ratione esse pariatur optio. In ducimus ad eaque itaque sunt molestiae 
-     labore est, nostrum mollitia dolorem dicta ullam deleniti sapiente possimus error, at excepturi atque voluptate quo, aspernatur dolore laboriosam quis asperiores neque? 
-     Accusantium, deserunt. Itaque id soluta, adipisci deleniti officia mollitia pariatur assumenda?
-     Molestiae ratione illum perspiciatis labore ex enim hic voluptatibus odit. Incidunt voluptates aspernatur earum cum ut possimus labore sapiente tempora.
-     orem ipsum dolor sit amet consectetur adipisicing elit. Hic, nam distinctio? Dolorem cupiditate fuga magnam a suscipit, officiis eveniet quas, 
-    mollitia ipsum culpa eius maxime dignissimos ducimus! Quaerat minus ab fugiat repellendus voluptatem similique quod ut distinctio doloremque, 
-    rem omnis voluptates soluta facilis cupiditate numquam exercitationem asperiores tempore eveniet, in cum, fugit recusandae? Debitis ab distinctio, 
-    necessitatibus nesciunt sequi eius rem laboriosam sed dignissimos. Rem reprehenderit eaque velit. Rerum, aliquam consectetur quam pariatur sit quidem error vel 
-    atque fugit totam, officia molestiae aut, nihil amet saepe optio voluptate iste voluptatum tempore quae nisi ea. Eum natus explicabo molestias dolore nihil repellendus
-     quaerat itaque! Eius, et. Labore optio expedita vero iusto totam numquam maiores adipisci perferendis nulla nisi commodi non excepturi amet vitae esse, ducimus veniam 
-     itaque rerum. Reprehenderit eius perspiciatis corporis ullam? Explicabo earum ducimus accusamus ratione esse pariatur optio. In ducimus ad eaque itaque sunt molestiae 
-     labore est, nostrum mollitia dolorem dicta ullam deleniti sapiente possimus error, at excepturi atque voluptate quo, aspernatur dolore laboriosam quis asperiores neque? 
-     Accusantium, deserunt. Itaque id soluta, adipisci deleniti officia mollitia pariatur assumenda?</Link>
+    <Navbar/>
+    <Footer/>
     </div>;
 }
