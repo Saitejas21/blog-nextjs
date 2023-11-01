15ef4c0bcab05244e449f4ffd9409a9f77583bfc
Sai Tejas A R <46528244+Saitejas21@users.noreply.github.com>
2023-11-01 11:29:32 +0530

    made mobile screen reponsiveness n add burger menu

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+"use client"
+
+import React, { useState } from 'react'
 import styles from './authLinks.module.css'
 import Link from 'next/link'
 
 function AuthLinks() {
+
+  const [open,setOpen] = useState(false)
   
   const status="notauthenticated"
   return (
     <>
     {status==="notauthenticated" ? (
+      <Link href="/login" className={styles.link}>Login</Link>
+
+    ):(
+      <>
+      <Link href="/write" className={styles.link}>Write</Link>
+      <span className={styles.link}>Logout</span>
+      </>
+    )}
+    <div className={styles.burger} onClick={()=>setOpen(!open)}>
+      <div className={styles.line}></div>
+      <div className={styles.line}></div>
+      <div className={styles.line}></div>
+    </div>
+    {open && (
+      <div className={styles.responsiveMenu}>
+        <Link href="/">Home</Link>
+        <Link href="/">About</Link>
+        <Link href="/">Contact</Link>
+        {status==="notauthenticated" ? (
       <Link href="/login">Login</Link>
 
     ):(
@@ -15,6 +38,8 @@ function AuthLinks() {
       <Link href="/write">Write</Link>
       <span className={styles.link}>Logout</span>
       </>
+    )}
+      </div>
     )}
     </>
   )
